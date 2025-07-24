cc7b6d42a4dc46953207f2b0cb84a1ba696543cf
KayembaJonathan <kayembejonathan5@gmail.com>
2025-07-24 16:33:44 +0200

    I have the navigation and fix some error

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ComponentNurse } from "./component-nurse/component-nurse";
 import { ComponentPatient } from "./component-patient/component-patient";
 import { ComponentReport } from "./component-report/component-report";
+import { Navbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ComponentNurse, ComponentPatient, ComponentReport],
+  imports: [RouterOutlet, ComponentNurse, ComponentPatient, ComponentReport, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
